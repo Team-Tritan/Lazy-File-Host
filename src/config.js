ec5a0d79ee7fc25df3e3b5d26c59bc9eb16ba1e6
Dylan J <dylanjames420@protonmail.com>
2022-02-22 16:28:20 -0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  port: 3000,
+  token: "fuckmedaddy69",
+
+  dirs: [
+    "esex",
+    "bigcock",
+    "suckingme",
+    "uwudaddy",
+    "hot-gay-porn",
+    "eboy",
+    "uwu-committee",
+    "thick-bitch-squad",
+    "nosleep",
+  ],
+};

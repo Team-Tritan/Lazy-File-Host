e0c82a6eae95f20d32290e0e517b47db0ea8da9f
Dylan J <dylan@suhd00d.co>
2023-05-21 01:32:59 -0400

    Create flameshot_uploader.sh

---
+++ flameshot_uploader.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+API_ENDPOINT="https://im.horny.rip/api/upload/"
+DOMAIN="https://im.horny.rip"
+API_KEY=""
+
+screenshot_file=$(mktemp).png
+flameshot gui -r > "$screenshot_file"
+response=$(curl -s -H "key: $API_KEY" -F "sharex=@$screenshot_file" "$API_ENDPOINT")
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to upload image."
+    rm "$screenshot_file" 
+    exit 1
+fi
+
+uploaded_url=$(echo "$response" | grep -oE '"url":"([^"]+)"' | cut -d'"' -f4)
+
+if [ -z "$uploaded_url" ]; then
+    echo "Error: Failed to retrieve uploaded URL."
+    rm "$screenshot_file"  
+    exit 1
+fi
+
+full_url="$DOMAIN/$uploaded_url"
+
+echo "Uploaded image URL: $full_url"
+echo -n "$full_url" | xclip -selection clipboard
+
+rm "$screenshot_file"

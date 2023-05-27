4be3d8ed23da72dc5b101618d2f05793520bf4d4
Dylan J <dylan@suhd00d.co>
2023-05-27 17:29:21 -0400

    Update index.ts

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response, NextFunction } from "express";
+import express, { Express, Request, Response } from "express";
 import fileUpload from "express-fileupload";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -6,28 +6,21 @@ import morgan from "morgan";
 import config from "./config";
 
 const app: Express = express();
-const loggerMiddleware = morgan("dev");
 
-const logger = (req: Request, res: Response, next: NextFunction) => {
-  loggerMiddleware(req, res, (err: any) => {
-    if (err) {
-      console.error(`[ERROR] ${err.stack}`);
-    }
-    next();
-  });
-};
+app.use(morgan("dev")); 
+app.use(cors()); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(fileUpload({ createParentPath: true }));
 
-app
-  .disable("x-powered-by")
-  .use(fileUpload({ createParentPath: true }))
-  .use(cors())
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(logger) // Use the custom logger middleware
-  .set("views", "pages")
-  .use("/api/content/raw", express.static("uploads"))
-  .set("view engine", "ejs")
-  .use("/", require("./routes/router"))
-  .listen(config.port, () => {
-    console.log(`[~~Daddy~~] Listening for fat cocks on port ${config.port}`);
-  });
+app.disable("x-powered-by");
+
+app.set("views", "pages");
+app.set("view engine", "ejs");
+
+app.use("/api/content/raw", express.static("uploads"));
+app.use("/", require("./routes/router"));
+
+app.listen(config.port, () => {
+  console.log(`[~~Daddy~~] Listening for fat cocks on port ${config.port}`);
+});

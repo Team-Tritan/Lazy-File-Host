35dcb13c9ac83fe79187e4f9906eaaa297bf38db
Dylan J <dylan@suhd00d.co>
2023-05-27 17:30:45 -0400

    Update index.ts

--- ts/index.ts
+++ ts/index.ts
@@ -4,13 +4,14 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import config from "./config";
+import router from "./routes/router"; 
 
 const app: Express = express();
 
 app.use(morgan("dev")); 
-app.use(cors()); 
+app.use(cors());
 app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload({ createParentPath: true }));
 
 app.disable("x-powered-by");
@@ -19,7 +20,7 @@ app.set("views", "pages");
 app.set("view engine", "ejs");
 
 app.use("/api/content/raw", express.static("uploads"));
-app.use("/", require("./routes/router"));
+app.use("/", router); // Use the imported router module
 
 app.listen(config.port, () => {
   console.log(`[~~Daddy~~] Listening for fat cocks on port ${config.port}`);

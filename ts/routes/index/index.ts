f60100d123adfb2ee7c2d4d9f90b717dce7ceb8b
Dylan J <dylan@suhd00d.co>
2023-05-27 17:13:05 -0400

    Update index.ts

--- ts/routes/index/index.ts
+++ ts/routes/index/index.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 import fs from "fs";
 import path from "path";
-import config from "../config";
+import config from "../../config";
 
 const router: Router = Router();
 

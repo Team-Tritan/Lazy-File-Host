8285aa9cc4fa61986acaa1741a1993aa831bd7c4
Dylan J <dylan@suhd00d.co>
2023-05-27 17:32:36 -0400

    Update index.ts

--- ts/index.ts
+++ ts/index.ts
@@ -8,20 +8,17 @@ import router from "./routes/router";
 
 const app: Express = express();
 
-app.use(morgan("dev")); 
-app.use(cors());
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload({ createParentPath: true }));
-
-app.disable("x-powered-by");
-
-app.set("views", "pages");
-app.set("view engine", "ejs");
-
-app.use("/api/content/raw", express.static("uploads"));
-app.use("/", router); // Use the imported router module
-
-app.listen(config.port, () => {
+app
+  .use(morgan("dev")) 
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(fileUpload({ createParentPath: true }))
+  .disable("x-powered-by")
+  .set("views", "pages")
+  .set("view engine", "ejs")
+  .use("/api/content/raw", express.static("uploads"))
+  .use("/", router)
+  .listen(config.port, () => {
   console.log(`[~~Daddy~~] Listening for fat cocks on port ${config.port}`);
 });

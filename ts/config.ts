1097eb5aacc74446a7f3085bf1bbfc6981a1c34d
Dylan J <dylan@suhd00d.co>
2023-05-27 17:07:44 -0400

    oops

---
+++ ts/config.ts
@@ -0,0 +1,13 @@
+interface Config {
+  port: number;
+  dirs: string[];
+  keys: string[];
+}
+
+const config: Config = {
+  port: 8080,
+  dirs: ["suicidey", "cock-vids", "kms", "fbi", "cia", "bricked", "666", "777"],
+  keys: ["fuckurmom"],
+};
+
+export default config;

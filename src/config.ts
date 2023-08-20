ee814abd8190da7d3fa032ef3bf00cf5aee38da2
big cock <you@example.com>
2023-08-20 19:29:45 -0400

    best

--- src/config.ts
+++ src/config.ts
@@ -18,6 +18,15 @@ const config: Config = {
     "https://ur.horny.rip",
     "https://probably.horny.rip",
     "https://because.horny.rip",
+    "https://img.cock.expert",
+    "https://img.femboys.porn",
+    "https://img.femboys.fyi",
+    "https://img.sexy.ong",
+    "https://img.highon.christmas",
+    "https://img.wank.group",
+    "https://img.cocks.dev",
+    "https://img.stoner.host",
+    "https://img.onlycats.ong",
   ],
 };
 

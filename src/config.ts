b1ad9413a1ca44ea62f916bcd2f245d95f7f8553
Dylan James <sysop@uploader.myinfra.lol>
2023-08-20 03:10:11 -0400

    pushing back

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ interface Config {
 const config: Config = {
   port: 8080,
   dirs: ["suicidey", "pp", "kms", "fbi", "cia", "666", "777"],
-  keys: ["fuckurmom"],
+  keys: ["fuckurmom2"],
 };
 
 export default config;
